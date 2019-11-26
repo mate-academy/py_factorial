d9a5b68f6e562c83ee2e17a875737d172dd1f6b5
Dima <dima.sapsay@gmail.com>
2019-11-26 15:14:20 +0200

    first commit

--- factorial.py
+++ factorial.py
@@ -1,3 +1,10 @@
-def fact(n: int) -> int:
+"""
+Calculate factorial without using loops and functions from standard library
+"""
 
-    return 42
+
+def fact(value: int) -> int:
+    """Factorial"""
+    if value == 0:
+        return 1
+    return fact(value - 1) * value
