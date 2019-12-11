545564f70331720e10a10d4247fd7d497429e993
zaikovlad95@gmail.com <Makepeacenotwar1995>
2019-12-12 01:22:13 +0200

    Return factorial of number without loops.

--- factorial.py
+++ factorial.py
@@ -1,3 +1,8 @@
-def fact(n: int) -> int:
+"""factorial module."""
 
-    return 42
+
+def fact(number: int) -> int:
+    """returns number's factorial."""
+    if number == 0:
+        return 1
+    return number * fact(number-1)
